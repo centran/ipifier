225807bf3243491fffd39bff0033b06bf2be5603
root <root@host1.ch1.int.bigwells.net>
2013-02-05 17:18:52 +0000

    login/logout pages

--- auth/views.py
+++ auth/views.py
@@ -1,22 +1,7 @@
 from django.shortcuts import render_to_response
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate 
+from django.http import HttpResponse
+from django.template.response import TemplateResponse
 
-def login_user(request):
-  state = "Please login..."
-  username = ''
-  password = ''
-  if request.POST:
-    username = request.POST.get('username')
-    password = request.POST.get('password')
-    
-    user = authenticate(username=username, password=password)
-    if user is not None:
-      if user.us_active:
-        login(request,user)
-        state = "Login successfull"
-      else:
-        state = "Your account is not active"
-    else:
-      state = "Username or password is incorrect"
-    
-  return render_to_response('auth.html',{'state':state, 'username': username})
+def logout(request):
+  return TemplateResponse(request, 'registration/logout.html')
--- ipifier/urls.py
+++ ipifier/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.contrib.auth.views import login, logout
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -6,5 +7,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^login/$', 'django.contrib.auth.views.login'),
+    url(r'^accounts/login/$', login),
+    url(r'^accounts/logout/$', logout, {'next_page': '/logout/'}),
+    url(r'^logout/$', 'auth.views.logout'),
 )
